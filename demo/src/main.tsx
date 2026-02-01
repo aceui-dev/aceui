f0422010f15002093b8f6f6249141666a5e17d91
vithuanaz <vithuanaz1995@gmail.com>
2026-02-01 12:58:09 +0530

    chore: clean up package-lock and update button component build process
    - Removed unused dependencies related to @react-aria and @react-stately from package.json.
    - Updated build script for the button component to copy CSS files directly instead of using copyfiles.
    - Removed unnecessary import of button styles in the demo application.

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./design-tokens.css";
 import "./index.css";
-import "@aceuidev/button/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
